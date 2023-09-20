6c1297746a1face6ba6c782bdb56e32d0b5615d1
Arijit Ghosh <92109154+Rancho2002@users.noreply.github.com>
2023-09-20 19:03:55 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,1 @@
-# This is a Python script for Challenge 20
-
-def main():
-    """
-    The main function prints a welcome message to the console.
-    """
-    print("Hello world, welcome to Challenge 20!")
-
-if __name__ == "__main__":
-    main()
+print("Hello world, welcome to Challenge 20!")
